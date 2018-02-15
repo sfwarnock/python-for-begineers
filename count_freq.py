d7f34d14a3952a5ebf8fac58b14f82675938f877
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-02-14 20:28:39 -0700

    Add files via upload

--- count_freq.py
+++ count_freq.py
@@ -2,8 +2,11 @@ list1 = ['and', 'and', 'but', 'for', 'too']
 
 sub = raw_input('Test: ')
 
-def freq_count(user_input,list):
-    for user_input in list:
-        print list, ":",list.count(sub);
+def freq_count(uis,nlist):
+    for word in nlist:
+        if sub in word:
+            print word, word.count(sub)
+        elif sub not in word:
+            print word, word.count(sub)
 
 freq_count(sub,list1)
