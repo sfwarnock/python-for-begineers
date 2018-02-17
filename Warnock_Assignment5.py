5c3caa44f4384aecb88c2fe06cc6f2acd939dba3
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-02-17 13:04:32 -0700

    Add files via upload

--- Warnock_Assignment5.py
+++ Warnock_Assignment5.py
@@ -2,15 +2,11 @@
 ## Scott Warnock
 ## Mesage frequency count
 
-#ufn = raw_input('Enter the text file name: ')
+ufn = raw_input('Enter the text file name: ')
 
-#ufp = 'C:/Users/Scott Warnock/Desktop/UCSD/Python/Python Code/'
+ufp = 'C:/Users/Scott Warnock/Desktop/UCSD/Python/Python Code/'
 
-#fhand = open(ufp+ufn)
-
-fhand = open('C:/Users/Scott Warnock/Desktop/UCSD/Python/Python Code/mbox.txt')
-
-f = []
+fhand = open(ufp+ufn)
 
 e = dict()
 
@@ -23,6 +19,14 @@ for line in fhand:
             e[email] = 1
         else:
             e[email] += 1
-print e
+    
+first = list()  
+
+for key, val in e.items():
+    first.append((val,key))
+
+first.sort(reverse=True)
+
+print first[0]
     
 fhand.close()
