23e069aaa20d7fd3efd5a525883012682d1896de
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-02-26 21:54:25 -0700

    Add files via upload

--- Week6/char_count.py
+++ Week6/char_count.py
@@ -10,9 +10,18 @@ size = 0
 
 c_count = 0 #total character count.
 
+site =' '
+
 while True:
     char = fhand.read(512)
-    c_count = c_count + len(char)
-    if c_count >= 3000:break
-    print char
+    if (len(char)<1):
+        break
+    site = site + char
+    c_count += len(char)
+fhand.close()
+
+site = site[:3000]
+
+print site
+
 print c_count
