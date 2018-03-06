399d82c52035e5a0b4bc961f5a5097156b1a7625
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-03-05 19:56:52 -0700

    Add files via upload

---
+++ Week7/geoxml.py
@@ -0,0 +1,30 @@
+# Assignement 7
+## Scott Warnock
+## Google Geocoding
+
+import urllib
+import time
+import xml.etree.ElementTree as ET
+import re
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
+
+while True:
+    time.sleep(2)
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+    
+    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    #print 'Retrieved',len(data),'characters'
+    #print data
+    tree = ET.fromstring(data)
+    
+    results = tree.findall('result')
+    for line in results:
+        line = line.rstrip()
+        x = re.findall('<short_name>([A-Z]+)<', line)
+        for code in x:
+            if re.findall('[A-Z]', code):print code
